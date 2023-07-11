dbcd832fbc5f3a3f54ff23147ad90577c7d04896
Aakash <parateakash21.ap@gmail,com>
2023-07-12 02:19:19 +0530

    created frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,116 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useTable } from 'react-table';
 
 function App() {
+  const [menuItems, setMenuItems] = useState([]);
+
+  useEffect(() => {
+    fetchMenuItems();
+  }, []);
+
+  const fetchMenuItems = async () => {
+    try {
+      const response = await axios.get('http://localhost:5000/api/menu');
+      setMenuItems(response.data);
+    } catch (error) {
+      console.log('Error fetching menu items:', error);
+    }
+  };
+
+  const savePrice = async (id, price) => {
+    try {
+      await axios.put(`http://localhost:5000/api/menu/${id}`, { price });
+      console.log('Price updated successfully');
+    } catch (error) {
+      console.log('Error updating price:', error);
+    }
+  };
+
+  const columns = React.useMemo(
+    () => [
+      { Header: 'Name', accessor: 'name' },
+      { Header: 'Image', accessor: 'image' },
+      { Header: 'Category', accessor: 'category' },
+      { Header: 'Label', accessor: 'label' },
+      { Header: 'Price', accessor: 'price', Cell: EditableCell },
+      { Header: 'Description', accessor: 'description' },
+    ],
+    []
+  );
+
+  const data = React.useMemo(() => menuItems, [menuItems]);
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable({ columns, data });
+
+  function EditableCell(cellProps) {
+    const { value: initialValue, row: { original } } = cellProps;
+    const [value, setValue] = useState(initialValue);
+
+    const onChange = (e) => {
+      setValue(e.target.value);
+    };
+
+    const onBlur = () => {
+      if (initialValue !== value) {
+        savePrice(original.id, value);
+      }
+    };
+
+    return <input value={value} onChange={onChange} onBlur={onBlur} />;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Restaurant Menu</h1>
+      <table {...getTableProps()} style={{ borderCollapse: 'collapse' }}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th
+                  {...column.getHeaderProps()}
+                  style={{
+                    padding: '8px',
+                    border: '1px solid black',
+                    fontWeight: 'bold',
+                  }}
+                >
+                  {column.render('Header')}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td
+                      {...cell.getCellProps()}
+                      style={{
+                        padding: '8px',
+                        border: '1px solid black',
+                      }}
+                    >
+                      {cell.render('Cell')}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
